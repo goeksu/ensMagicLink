c64117949abf6b66a6a51a19f3e655c3d3eab01c
Ahmet Göksu <ahmet@goksu.in>
2023-11-18 12:26:24 +0300

    avatar resolved, style seperated

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_file
 import qrcode
 from io import BytesIO
+from web3 import Web3
 
 app = Flask(__name__)
 
@@ -8,12 +9,17 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         ens_name = request.form.get('ens_name')
-        return render_template('hello.html', ens_name=ens_name)
+        w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/90a62b0b31bc4303be06952875fde341'))
+        address = w3.ens.address(ens_name)
+        avatar = w3.ens.get_text(ens_name, "avatar")
+
+        return render_template('hello.html', ens_name=ens_name,address=address, avatar=avatar)
 
     return render_template('index.html')
 
 @app.route('/qr_code/<ens_name>')
 def qr_code(ens_name):
+
     url = f"https://app.ens.domains/{ens_name}"
     qr = qrcode.QRCode(
         version=1,
